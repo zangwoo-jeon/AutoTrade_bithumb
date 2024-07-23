19ac3a42964fc9ceb6493690cc3fd51f6a44b46e
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2024-07-23 20:31:57 +0900

    run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ my_ticker = "Write coin ticker here"
 
 #목표값 계산
 def get_target_price(ticker, k):
-    df = pybithumb.get_ohlcv(ticker)[-2:]
+    df = pybithumb.get_ohlcv(ticker)[-1:]
     target_price = df.iloc[0]['low'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
@@ -30,9 +30,9 @@ def get_ror(ticker, k):
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['low'] + df['range'].shift(1)
 
-    fee = 0.0032
+    fee = 0.04
     df['ror'] = np.where(df['high'] > df['target'],
-                         (df['high']*0.95) / df['target'] - fee,
+                         df['close'] / df['target'] - fee,
                          1)
 
     ror = df['ror'].cumprod().iloc[-2]
@@ -65,12 +65,13 @@ def sell_crypto_currency(ticker):
     My.sell_market_order(ticker, unit)
 
 
-#high_price : 매수한 이후의 코인의 최고가, buy_price : 매수값, sell_price : 매도값, cal_k : bk 값 갱신 주기, bk : 수익률 계산 함수에서 계산한 최적의 k 값
-high_price = 0
+#buy_price : 매수값, sell_price : 매도값, low_price : 매수 전 최저가 cal_k : bk 값 갱신 주기, bk : 수익률 계산 함수에서 계산한 최적의 k 값
 buy_price = 0
 sell_price = 100000
+low_price = 100000
 cal_k = 70000
 bk = find_best_k(my_ticker)
+flag = True
 
 while True:
     try:
@@ -85,23 +86,23 @@ while True:
         current_price = get_current_price(my_ticker)
         Krw = My.get_balance(my_ticker)[2]
         MY_unit = My.get_balance(my_ticker)[0]
+        TH_price = sell_price * 0.97
         #print("buy price : ", buy_price, "Krw : ", Krw)
         #print("보유 코인수 : ", MY_unit)
         # 매수가 되었다면 최고가 갱신
-        if buy_price > 0:
-            high_price = max(high_price, current_price)
+        if buy_price == 0:
+            low_price = min(low_price, current_price)
         # TH_price : 다시 매수할 때에는 최근에 매도한 가격의 97% 가격을 임계값으로 설정
-        TH_price = sell_price*0.97
         # 만약 TH_price가 Target_price나 ma5의 최대값보다 작으면 매수가 안되므로 그 값의 1.02값으로 변경
-        if TH_price < max(Target_price, ma5) and current_price < max(Target_price, ma5):
-            TH_price = max(Target_price, ma5)*1.02
+        if flag == False and current_price >= low_price * 1.03:
+            flag = True
         #print("target price : ", Target_price, "ma5 : ", ma5)
         #print("current_price : ", current_price, "high price : ", high_price)
 
         # 만약 내가 매수를 하지 않았으면 매수 진행
         if buy_price == 0:
             # 현재가가 Target_price와 ma5 이상이면 상승장에 진입했다고 판단함. 그리고 TH_price보다 작으면 매수 진행
-            if Target_price < current_price and ma5 < current_price and current_price < TH_price:
+            if Target_price < current_price and ma5 < current_price and flag == True:
                 #현재 내가 보유한 원화량의 70%를 매수. 이유는 모르겠으나 이 가격이상으로는 오류가 발생해서 매수가 안됨
                 #혹시 매수할 금액을 변경하고 싶으면 buy_order_amount를 바꿔주면 된다.
                 #ex. 10만원어치를 매수하고 싶으면 buy_order_amount = 100000  
@@ -111,27 +112,23 @@ while True:
                 #print("매수 금액 : ", current_price)
                 buy_crypto_currency(buy_order_amount, my_ticker)
                 buy_price = current_price
+                low_price = 100000
         
         #내가 매수를 진행했으면 매도 진행
         elif buy_price > 0 and MY_unit > 0:
             #last_buy_price : 현재 매수가, hand_cut_price : 손절가, 매수가에서 3%이상 떨어지면 손절, profit_price : 익절가, 최고가 기준 3%이상 떨어지면 익절
             last_buy_price = buy_price
-            #hand_cut_price = last_buy_price * 0.97
             hand_cut_price = last_buy_price * 0.97
-            profit_price = high_price * 0.97
+            profit_price = last_buy_price * 1.05
             
-            # 만약 현재 최고가 갱신 중이면 매도하지 않고 존버
-            if current_price == high_price:
-                continue
-
             #손절가 이하로 떨어지면 매도도
-            elif current_price < hand_cut_price:
+            if current_price < hand_cut_price:
                 #print("손절합니다.")
                 #print("손절가 : ", current_price)
                 sell_crypto_currency(my_ticker)
                 sell_price = current_price
                 buy_price = 0
-                high_price = 0
+                flag = False
 
             #익절가 이하로 떨어지면 익절
             elif last_buy_price < current_price < profit_price:
@@ -140,7 +137,6 @@ while True:
                 sell_crypto_currency(my_ticker)
                 sell_price = current_price
                 buy_price = 0
-                high_price = 0
 
         time.sleep(1)
 
