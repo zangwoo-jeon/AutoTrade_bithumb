47648625c7a6c32f42521ea9297d500858693efb
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2024-08-03 02:06:08 +0900

    exercise.tf

---
+++ exercise.tf
@@ -0,0 +1,24 @@
+resource "google_compute_instance" "default" {
+  name         = "cointrade"
+  machine_type = "e2-medium"
+  zone         = "asia-northeast3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    sudo apt-get install git
+    git clone https://github.com/zangwoo-jeon/AutoTrade_bithumb.git
+    sudo apt update
+    sudo apt-get install -y python3-pip
+    pip install pybithumb
+    EOF
+    
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
